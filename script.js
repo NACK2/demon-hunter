d69b94f5eab58d274b79b189b7be245ab76f367b
nick <nicholaskang5@gmail.com>
2023-07-03 18:53:31 -0700

    - Pressing play now directs user to a different screen

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const mainMenu = document.querySelector("#main-menu");
+const gameMenu = document.querySelector("#game-menu");
 const playBtn = document.querySelector("#play-btn");
 const socialMediaBtns = document.querySelector("#social-menu");
 playBtn.onclick = play;
@@ -6,4 +7,5 @@ playBtn.onclick = play;
 function play() {
     mainMenu.style.display = "none";
     socialMediaBtns.style.display = "none";
+    gameMenu.style.display = "block";
 }
