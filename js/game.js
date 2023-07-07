c74dcb3edd38f18bff420a525839f8f6b56f9806
nick <nicholaskang5@gmail.com>
2023-07-06 20:11:17 -0700

    - All successful and non-successful health and weapon purchases appear in console text - Made use of array of objects for weapons - If user already has a weapon in their inventory, purchase fails

--- js/game.js
+++ js/game.js
@@ -3,30 +3,29 @@ let gold = 70;
 let xp = 0;
 let inventory = [];
 
-let weapons = [
+let weaponsList = [
     {
-        // btn click for buying weapon will send a paremeter matching these names
         name: "slingshot",
         price: 10,
         power: 5
     },
     {
-        name: "baseballBat",
+        name: "baseball bat",
         price: 30,
         power: 10
     },
     {
-        name: "woodSword",
+        name: "wood sword",
         price: 60,
         power: 30
     },
     {
-        name: "stoneSword",
+        name: "stone sword",
         price: 90,
         power: 50
     },
     {
-        name: "dualWield",
+        name: "dual wield sword",
         price: 150,
         power: 80
     }
@@ -38,10 +37,10 @@ const btn2 = document.querySelector("#btn2");
 const btn3 = document.querySelector("#btn3");
 
 const dialogueText = document.querySelector("#dialogueText");
-const healthTxt = document.querySelector("#healthText");
-const goldTxt = document.querySelector("#goldText");
-const xpTxt = document.querySelector("#xpText");
-const consoleTxt = document.querySelector("#consoleText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const xpText = document.querySelector("#xpText");
+const consoleText = document.querySelector("#consoleText");
 const weaponsMenu = document.querySelector("#weaponsMenu");
 
 // init buttons
@@ -88,29 +87,43 @@ function buyHealth() {
     weaponsMenu.style.display = "none";
     dialogueText.style.display = "block";
     if (gold < 10) {
-        dialogueText.innerText = "Insufficient gold!";
+        consoleText.innerText = "Insufficient gold!";
     }
     else {
         health += 10;
         gold -= 10;
 
-        dialogueText.innerText = "Successfully gained 10 health!";
+        consoleText.innerText = "Successfully bought 10 health!";
         healthText.innerText = health;
         goldText.innerText = gold;
     }
 }
 
 function buyWeaponsMenu() {
-    dialogueText.innerText = "You may scroll down for more weapons!"
+    dialogueText.style.display = "none";
+    consoleText.innerText = "You may scroll down for more weapons!"
     weaponsMenu.style.display = "block";
 }
 
-function buyWeapon(weapon) {
-    // look for weapon object matching the weapon paremeter, add it to inventory
-    for (let i = 0; i<weapons.length; ++i) { 
-        if (weapons[i].name == weapon) {
-            inventory.push(weapons[i]);
-            console.log(inventory);
+function buyWeapon(weaponToBuy) {
+    // weaponsList is array of objects, each object is info on a weapon
+    // loop through weaponsList, find the object w/ name matching weaponToBuy, 
+    // and add it to inventory as long as user doesnt already have it
+    for (let i = 0; i<weaponsList.length; ++i) { 
+        if (weaponsList[i].name == weaponToBuy) { // found matching weapon 
+            let hasWeapon = inventory.includes(weaponsList[i], 0); // check if user already has weapon
+            if (hasWeapon) {
+                consoleText.innerText = "Weapon already bought!";
+            }
+            else if (weaponsList[i].price > gold) { 
+                consoleText.innerText = "Insufficient gold!"
+            }
+            else { // successfully buying
+                gold -= weaponsList[i].price;
+                inventory.push(weaponsList[i]);
+                goldText.innerText = gold;
+                consoleText.innerText = "Successfully bought a " + weaponToBuy + "!";
+            }
         }
     }
 }
