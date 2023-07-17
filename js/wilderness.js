c6bae6c3c74783528fb537e2f4b2fcb1efe247d1
nick <nicholaskang5@gmail.com>
2023-07-17 15:20:23 -0700

    -Added randomized bush spawning

--- js/wilderness.js
+++ js/wilderness.js
@@ -1,9 +1,22 @@
 // WILDNERESS IS INSPIRED BY POKEMON
 
+const NUM_BUSHES = 15;
 const exitBtn = document.querySelector("#exitBtn");
+const land = document.querySelector("#land");
 
 exitBtn.onclick = gameMenu;
 
 function gameMenu() {
     window.location = "game.html";
 }
+
+function createBushes() {
+    for (let i=0; i<NUM_BUSHES; ++i) { 
+        const div = document.createElement("div"); // creating element 
+        div.id = "bush"; // giving element the bush id
+        div.style.left = Math.random() * 100 + "%"; // random distance from left and top of screen
+        div.style.top = Math.random() * 100 + "%";
+        land.appendChild(div);
+    }
+}
+createBushes();
